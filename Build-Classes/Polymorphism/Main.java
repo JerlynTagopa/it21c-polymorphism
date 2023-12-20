c75183e0247a63a3d768a5a7b7abb359e14378ad
Jerlyang <146070025+JerlynTagopa@users.noreply.github.com>
2023-12-20 21:35:57 +0800

    Added Polymorphism package

---
+++ Build-Classes/Polymorphism/Main.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        polymorphism_color my_color= new polymorphism_color();
+        polymorphism_pink my_pink = new polymorphism_pink();
+        polymorphism_favorite my_favorite = new polymorphism_favorite();
+        
+        System.out.println("My Favorite is Pink");
+        System.out.println("Forever Fave");
+        System.out.println("Black,Pink");
+        
+        
+    }
+    
+}
