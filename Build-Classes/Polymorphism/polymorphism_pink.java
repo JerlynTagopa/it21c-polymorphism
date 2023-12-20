77f604783cdcf75606d6dc35d4c1bbfcf070f970
Jerlyang <146070025+JerlynTagopa@users.noreply.github.com>
2023-12-20 21:37:38 +0800

    Added Polymorphism package

---
+++ Build-Classes/Polymorphism/polymorphism_pink.java
@@ -0,0 +1,11 @@
+
+package Polymorphism;
+
+
+class polymorphism_pink {
+    public void color(){
+        System.out.println("Rose");
+        System.out.println("1 dozen of pink rose");
+    }
+    
+}
